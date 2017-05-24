f9b51621d59757598b3036e6c3e7de781fb77378
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-05-24 14:57:23 +0700

    first version

---
+++ FoldAnArray/kata.cpp
@@ -0,0 +1,16 @@
+/**
+ * \author tungdt9 thanhtungpfiev@gmail.com
+ *
+ * \date 5/24/2017
+ * \class Kata
+ *
+ * \brief
+ *
+ *
+ */
+#include "kata.h"
+
+Kata::Kata()
+{
+
+}
---
+++ FoldAnArray/kata.h
@@ -0,0 +1,21 @@
+/**
+ * \author tungdt9 thanhtungpfiev@gmail.com
+ *
+ * \date 5/24/2017
+ * \class Kata
+ *
+ * \brief
+ *
+ *
+ */
+#ifndef KATA_H
+#define KATA_H
+
+
+class Kata
+{
+public:
+    Kata();
+};
+
+#endif // KATA_H
