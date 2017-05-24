72fc97645145bfb670c02f3cc33bb6bf20db09a8
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-05-24 10:59:38 +0700

    first version

---
+++ SumOfNumbersFrom0toN/sequencesum.cpp
@@ -0,0 +1,16 @@
+/**
+ * \author tungdt9 thanhtungpfiev@gmail.com
+ *
+ * \date 5/24/2017
+ * \class SequenceSum
+ *
+ * \brief
+ *
+ *
+ */
+#include "sequencesum.h"
+
+SequenceSum::SequenceSum()
+{
+
+}
---
+++ SumOfNumbersFrom0toN/sequencesum.h
@@ -0,0 +1,21 @@
+/**
+ * \author tungdt9 thanhtungpfiev@gmail.com
+ *
+ * \date 5/24/2017
+ * \class SequenceSum
+ *
+ * \brief
+ *
+ *
+ */
+#ifndef SEQUENCESUM_H
+#define SEQUENCESUM_H
+
+
+class SequenceSum
+{
+public:
+    SequenceSum();
+};
+
+#endif // SEQUENCESUM_H
