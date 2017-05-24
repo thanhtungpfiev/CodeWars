b6d0a4bec1d8276acb3670988013e9bbbf8b3834
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-05-24 14:40:38 +0700

    update

--- Sorted/main.cpp
+++ Sorted/main.cpp
@@ -9,8 +9,55 @@
  *
  */
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+std::string is_sorted_and_how(std::vector<int> array)
+{
+    std::string result;
+    if (array.size() == 2) {
+      if (array.at(0) > array.at(1)) {
+        result = "yes, descending";
+      } else {
+        result = "yes, ascending";
+      }
+    }
+    bool isSorted = true;
+    bool isAscending = true;
+    if (array.size() > 2) {
+      if (array.at(0) < array.at(1)) {
+        isAscending = true;
+      } else {
+        isAscending = false;
+      }
+      if (isAscending == true) {
+        for (unsigned int i = 1; i < array.size() - 1; ++i) {
+          if (array.at(i) > array.at(i + 1))  {
+            isSorted = false;
+            break;
+          }
+        }
+      } else {
+        for (unsigned int i = 1; i < array.size() - 1; ++i) {
+          if (array.at(i) < array.at(i + 1)) {
+            isSorted = false;
+            break;
+          }
+        }
+      }
+    }
+    if (isSorted == false) {
+      result = "no";
+    } else {
+      if (isAscending == true) {
+        result = "yes, ascending";
+      } else {
+        result = "yes, descending";
+      }
+    }
+    return result;
+}
 
 int main(int argc, char *argv[])
 {
