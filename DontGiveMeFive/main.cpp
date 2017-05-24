dd7d82d3f35fe2ea8aa218cb1a1649de89f690fd
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-05-24 16:19:36 +0700

    DontGiveMeFive update main

--- DontGiveMeFive/main.cpp
+++ DontGiveMeFive/main.cpp
@@ -12,8 +12,21 @@
 
 using namespace std;
 
+int dontGiveMeFive(int start, int end)
+{
+  int count = 0;
+  for (int i = start; i <= end; ++i) {
+    std::string s = std::to_string(i);
+    std::size_t found = s.find("5");
+    if (found == std::string::npos) {
+      count++;
+    }
+  }
+  return count;
+}
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    cout << dontGiveMeFive(1, 10) << endl;
     return 0;
 }
