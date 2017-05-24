1af98ee7871fa0027e587c9cb87f35ce8a702966
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-05-24 17:08:07 +0700

    CompareStringsBySumOfChars second version

--- CompareStringsBySumOfChars/main.cpp
+++ CompareStringsBySumOfChars/main.cpp
@@ -13,43 +13,26 @@
 
 using namespace std;
 
-void uppercaseString(std::string &str)
-{
-  std::transform(str.begin(), str.end(), str.begin(), ::toupper);
-}
-
-bool checkContainNonAlpha(const std::string &str)
-{
-  return std::find_if(str.begin(), str.end(), [](char c) { return !std::isalpha(c); }) != str.end();
-}
-
 long totalCharString(const std::string &str)
 {
   long sum = 0;
-  for (unsigned int i = 0; i < str.size(); ++i) {
-    sum += int(str.at(i));
+  for (const auto &c : str) {
+      if (std::isalpha(c)) {
+          sum += int(std::toupper(c));
+      } else {
+          sum = 0;
+      }
   }
   return sum;
 }
 
 bool compare(std::string s1, std::string s2)
 {
-    uppercaseString(s1);
-    uppercaseString(s2);
-    if (s1.empty() || checkContainNonAlpha(s1)) {
-      s1 = "";
-    }
-    if (s2.empty() || checkContainNonAlpha(s2)) {
-      s2 = "";
-    }
-    if (!s1.compare(s2)) {
-      return true;
-    }
-    return totalCharString(s1) == totalCharString(s2) ? true : false;
+    return totalCharString(s1) == totalCharString(s2);
 }
 
 int main(int argc, char *argv[])
 {
-    cout << compare("AD", "BC") << endl;
+    cout << compare("gf", "FG") << endl;
     return 0;
 }
