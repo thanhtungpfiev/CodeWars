4a031a207defe88d5b260adc6d0bdf23d70c6567
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-06-03 11:43:59 +0700

    FoldArray update test case

--- FoldAnArray/main.cpp
+++ FoldAnArray/main.cpp
@@ -22,5 +22,51 @@ int main(int argc, char *argv[])
         cout << e << " ";
     }
     cout << endl;
+
+    /* Test case
+    Describe(Tests)
+    {
+        It(ExampleTest1)
+        {
+            std::vector<int> expected = { 6, 6, 3 };
+
+            Kata kata;
+            std::vector<int> actual = kata.foldArray({ 1, 2, 3, 4, 5 }, 1);
+
+            Assert::That(actual, Is().EqualTo(expected));
+        }
+
+        It(ExampleTest2)
+        {
+            std::vector<int> expected = { 9, 6 };
+
+            Kata kata;
+            std::vector<int> actual = kata.foldArray({ 1, 2, 3, 4, 5 }, 2);
+
+            Assert::That(actual, Is().EqualTo(expected));
+        }
+
+        It(ExampleTest3)
+        {
+            std::vector<int> expected = { 15 };
+
+            Kata kata;
+            std::vector<int> actual = kata.foldArray({ 1, 2, 3, 4, 5 }, 3);
+
+            Assert::That(actual, Is().EqualTo(expected));
+        }
+
+        It(ExampleTest4)
+        {
+            std::vector<int> expected = { 14, 75, 0 };
+
+            Kata kata;
+            std::vector<int> actual = kata.foldArray({ -9, 9, -8, 8, 66, 23 }, 1);
+
+            Assert::That(actual, Is().EqualTo(expected));
+        }
+    };
+    */
+
     return 0;
 }
