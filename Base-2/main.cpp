8c82f1e72dd558c20a5ee682192a01249efb6b97
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-06-03 11:33:16 +0700

    Base-2 update test case

--- Base-2/main.cpp
+++ Base-2/main.cpp
@@ -18,5 +18,14 @@ int main(int argc, char *argv[])
 {
     cout << Negabinary::ToNegabinary(0) << endl;
     cout << Negabinary::ToInt("10110") << endl;
+
+    /* Test case
+    test.assert_equals(int_to_negabinary(6), '11010')
+    test.assert_equals(int_to_negabinary(-6), '1110')
+
+    test.assert_equals(negabinary_to_int('11010'), 6)
+    test.assert_equals(negabinary_to_int('1110'), -6)
+    */
+
     return 0;
 }
