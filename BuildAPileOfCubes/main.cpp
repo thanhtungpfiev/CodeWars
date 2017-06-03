d91cf4f4dd17ae0d884139c265027369068914bf
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-06-03 11:35:09 +0700

    BuildAPileOfCubes update test case

--- BuildAPileOfCubes/main.cpp
+++ BuildAPileOfCubes/main.cpp
@@ -21,5 +21,14 @@ int main(int argc, char *argv[])
     cout << ASum::findNb(135440716410000) << endl;
     cout << ASum::findNb(40539911473216) << endl;
     cout << ASum::findNb(26825883955641) << endl;
+
+    /* Test case
+    test.assert_equals(find_nb(4183059834009), 2022)
+    test.assert_equals(find_nb(24723578342962), -1)
+    test.assert_equals(find_nb(135440716410000), 4824)
+    test.assert_equals(find_nb(40539911473216), 3568)
+    test.assert_equals(find_nb(26825883955641), 3218)
+    */
+
     return 0;
 }
