808b2326282654ed1f3e223d6e19527eca7cdcc5
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-06-03 11:57:13 +0700

    PI approximation update test case

--- PIapproximation/main.cpp
+++ PIapproximation/main.cpp
@@ -17,5 +17,18 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout << PiApprox::iterPi(0.1) << endl;
+
+    /* Test case
+    Describe(PiapproxTest)
+    {
+        It(test1)
+        {
+            string sol1 = "[10, 3.0418396189]";
+            string ans1 = PiApprox::iterPi(0.1);
+            Assert::That(ans1, Equals(sol1));
+        }
+    };
+    */
+
     return 0;
 }
