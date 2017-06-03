1fb650523ed38ca5868ee89a62261b22af810c8e
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2017-06-03 21:31:32 +0700

    SumOfNumbersFrom0toN update test case

--- SumOfNumbersFrom0toN/main.cpp
+++ SumOfNumbersFrom0toN/main.cpp
@@ -12,5 +12,17 @@ int main(int argc, char *argv[])
     cout << s2.showSequence() << endl;
     SequenceSum s3(6);
     cout << s3.showSequence() << endl;
+
+    /* Test case
+    Describe(SequenceSumTest)
+    {
+        It(testBasic)
+        {
+            SequenceSum seqsum (6);
+            Assert::That(seqsum.showSequence(), Equals("0+1+2+3+4+5+6 = 21"));
+        }
+    };
+    */
+
     return 0;
 }
