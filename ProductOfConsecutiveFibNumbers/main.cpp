ac7b5fc6690dda65c355e73211c967d37796dde2
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2017-06-03 21:16:46 +0700

    ProductOfConsecutiveFibNumbers update test case

--- ProductOfConsecutiveFibNumbers/main.cpp
+++ ProductOfConsecutiveFibNumbers/main.cpp
@@ -38,5 +38,28 @@ int main(int argc, char *argv[])
         cout << e << " ";
     }
     cout << endl;
+
+    /* Test case)
+    typedef unsigned long long ull;
+    void testequal(std::vector<ull> ans, std::vector<ull> sol)
+    {
+        Assert::That(ans, Equals(sol));
+    }
+    void dotest(ull prod, std::vector<ull> expected)
+    {
+        testequal(ProdFib::productFib(prod), expected);
+    }
+    Describe(productFib_Tests)
+    {
+        It(Fixed__Tests)
+        {
+            dotest(4895, {55, 89, true});
+            dotest(5895, {89, 144, false});
+            dotest(74049690, {6765, 10946, 1ULL});
+            dotest(84049690, {10946, 17711, 0ULL});
+        }
+    };
+    */
+
     return 0;
 }
