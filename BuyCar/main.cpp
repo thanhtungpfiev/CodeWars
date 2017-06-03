0138821cb9f77168258027038e25fdeb572fed87
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-06-03 11:37:33 +0700

    BuyCar update test case

--- BuyCar/main.cpp
+++ BuyCar/main.cpp
@@ -19,5 +19,28 @@ int main(int argc, char *argv[])
 {
     cout << "Hello World!" << endl;
     BuyCar::nbMonths(2000, 8000, 1000, 1.5);
+
+    /* Test case
+    using namespace std;
+
+    Describe(BuycarTest)
+    {
+        It(test1)
+        {
+          static const int r1[] = { 6, 766 };
+          vector<int> s (r1, r1 + sizeof(r1) / sizeof(r1[0]));
+          vector<int> ans1 = BuyCar::nbMonths(2000, 8000, 1000, 1.5);
+          Assert::That(ans1, Equals(s));
+        }
+        It(test2)
+        {
+          static const int r1[] = { 0, 4000 };
+          vector<int> s (r1, r1 + sizeof(r1) / sizeof(r1[0]));
+          vector<int> ans1 = BuyCar::nbMonths(12000, 8000, 1000, 1.5);
+          Assert::That(ans1, Equals(s));
+        }
+    };
+    */
+
     return 0;
 }
